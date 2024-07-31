40d034424408c6c8842661f412963e0184094756
krayush1109 <hellotoayush@gmail.com>
2024-07-31 11:57:03 +0530

    ui improved

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -5,7 +5,7 @@ const Gallery = () => {
     const [allPics, setALlPics] = useState([]);
 
     const getImages = async() => {
-        const response = await axios.get('/list?page=1&limit=20')
+        const response = await axios.get('/products?limit=5')
         console.log(response.data);
         setALlPics(response.data);
     }
@@ -23,14 +23,13 @@ const Gallery = () => {
       <>
         <button onClick={handleClick} className='px-3 py-1 bg-blue-500 cursor-pointer text-white font-extrabold'>Fetch Images</button>
 
-          <div className="card-container flex">
+          <div className="card-container container flex space-x-12 p-5">
               {allPics.map((pic) => {
                   console.log(pic.url);
                   return (
-                      <>
-                      <img key={pic.id} src={pic.url} alt="" height="200px" width="200px" />
-                      
-                      </>
+                      <div className='border-slate-200 border-2 p-5 shadow-md hover:shadow-lg hover:-translate-y-2 duration-200 cursor-pointer'>
+                      <img key={pic.id} src={pic.image} alt="" height="200px" width="200px" />                      
+                      </div>
                   )
               })  }
           </div>
--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://picsum.photos/v2/",
+    baseURL: "https://fakestoreapi.com/",
     timeout: 10000, // Optional: Set a timeout for requests
     headers: {
         'Content-Type': 'application/json', // Set default headers
